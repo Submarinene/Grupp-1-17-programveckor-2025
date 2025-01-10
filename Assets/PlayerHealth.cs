5bf84e85c4f3c67845c58734ef9eb6b2131e6984
EstellaBriones <estella.vallecerabriones@elev.ga.lbs.se>
2025-01-10 13:11:00 +0100

    la till player health script

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int maxHealth = 3;
+    int health;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void Hurt(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene(1);
+    }
